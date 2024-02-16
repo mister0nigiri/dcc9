261faae1ecd1a154cbd799656d77cb8fa792639a
miyashun <gongyongjunyou@gmail.com>
2024-02-16 23:05:04 +0900

    会社情報ページのCSS完了

--- js/main.js
+++ js/main.js
@@ -19,3 +19,45 @@ const mySwiper = new Swiper ('.swiper', {
   },
 });
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var navLinks = document.querySelectorAll('.company-nav ul li a');
+
+  function setActiveLink() {
+    var currentScroll = window.scrollY;
+
+    navLinks.forEach(function(link) {
+      var sectionId = link.getAttribute('href').substring(1);
+      var section = document.getElementById(sectionId);
+
+      if (section.offsetTop <= currentScroll && section.offsetTop + section.offsetHeight > currentScroll) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  }
+
+  function scrollToSection(event) {
+    event.preventDefault();
+
+    var targetId = this.getAttribute('href').substring(1);
+    var targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  setActiveLink();
+
+  window.addEventListener('scroll', setActiveLink);
+
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', scrollToSection);
+  });
+});
